0758ed515ef95cd11f49fa5999fbaa8df5f35e59
Le Phu Quoc <tygovip119@gmail.com>
2022-04-17 15:31:22 +0700

    Update layouts

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './DrawerNavigator';
+import NotificationNativeStackNavigator from './NotificationsNativeStackNavigator';
 
 function AppNavigation() {
   return (
     <NavigationContainer>
-      <DrawerNavigator />
+      <NotificationNativeStackNavigator />
     </NavigationContainer>
   );
 }
