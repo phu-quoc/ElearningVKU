af1c821e608a018a6a4524c3b4639a7d62c45895
Le Phu Quoc <tygovip119@gmail.com>
2022-05-23 10:53:55 +0700

    Prepare login by google

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 
 function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/icons/logovku.png')} style={styles.image} />
       <Text>Name</Text>
       <Text>Password</Text>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 200,
+    height: 100
+  }
+});
 export default LoginScreen;
